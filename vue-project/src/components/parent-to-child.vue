27061fb57545918148d90922f326b89aea371465
Vaibhavrahinj <70082433+Vaibhavrahinj@users.noreply.github.com>
2022-07-22 18:22:02 +0530

    parent.vue child.vue parent-to-child.vue files

---
+++ vue-project/src/components/parent-to-child.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>Send data parent to child</h1>
+<Child name="Vaibhav Rahinj" :user="user" :getData="getData"></Child>
+</template>
+
+<script lang="ts">
+import Child from './child.vue'
+export default {
+    name: "parentToChildVue",
+    component: {
+        Child
+    },
+    data() {
+        return {
+            user: {name: 'sai', email: 'sai@gmail.com'}
+        }
+    },
+
+methods: {
+    getData() {
+        console.log("Parent to child ")
+    }
+}
+
+}
+</script>
